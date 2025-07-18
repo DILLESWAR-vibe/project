3113d406fed0590203e8710343e84b825ca3ffd4
DILLESWAR-vibe <dilli.koda284@gmail.com>
2025-07-18 10:52:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/project/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ["09b8-103-120-51-102.ngrok-free.app"],
