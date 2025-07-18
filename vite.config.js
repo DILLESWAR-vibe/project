0abeea6ee064ec4767445b3b6455d532fb53d0ae
dilleswar3 <dilli.koda284@gmail.com>
2025-07-18 11:11:46 +0530

    Updated config for correct base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/project/',
+  base: process.env.BASE_PATH || '/',
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ["09b8-103-120-51-102.ngrok-free.app"],
